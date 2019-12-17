22300d569a7286403190f7e22de1910dd44ee2e2
Anup Arakkal <anup.anup2001@gmail.com>
2019-12-16 21:27:03 -0500

    Initial commit

---
+++ src/main/java/com/example/fixplayground/FixplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.fixplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fixplayground/FixplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fixplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
